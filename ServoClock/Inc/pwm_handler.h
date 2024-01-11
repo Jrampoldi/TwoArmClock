4be6a72f1d8caee3f8c98daac6ac291aa4227d4c
Jourdan Rampoldi <104399321+Jrampoldi@users.noreply.github.com>
2024-01-11 10:47:19 -0700

    Update pwm_handler.h
    Added descriptions and the map_to_servo function declaration.

--- ServoClock/Inc/pwm_handler.h
+++ ServoClock/Inc/pwm_handler.h
@@ -1,5 +1,5 @@
 /*
- * pmw_handler.h
+ * pwm_handler.h
  *
  *  Created on: Dec 25, 2023
  *      Author: jourdanrampoldi
@@ -10,6 +10,14 @@
 
 #include "stm32f411xe.h"
 
+/* Function Declarations*/
 void pwm_tim2_init(void);
+// Description: initializes tim2 to output a pwm signal to 
+// channel 2, 3, & 4, which are configured to pins 
+// 1, 2, & 3.
+
+double map_to_servo(double min_range, double max_range, double value);
+// Description: Takes min_range, max_range, and a value, then returns mapped value
+// to relative servo range. Servo range definitions in pwm_handler.c.
 
 #endif /* PWM_HANDLER_H_ */
