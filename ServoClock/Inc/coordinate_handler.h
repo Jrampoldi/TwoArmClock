9b708872cc59c02bd3507e7d23e126f25ab61b18
Jourdan Rampoldi <104399321+Jrampoldi@users.noreply.github.com>
2024-01-12 13:10:00 -0700

    Update coordinate_handler.h
    Added move_arm declaration with description. Added libraries for coordinate_handler.c file.

--- ServoClock/Inc/coordinate_handler.h
+++ ServoClock/Inc/coordinate_handler.h
@@ -8,6 +8,14 @@
 #ifndef COORDINATE_HANDLER_H_
 #define COORDINATE_HANDLER_H_
 
+/* include headers */
+#include <math.h>
+#include "pwm_handler.h"
 
+/* Function Declarations */
+void move_arm(double x, double y);
+// Description: takes in a coordinate value, converts coordinate 
+// to degrees in radians, converts radians to degrees, then 
+// sends a move_servo request with mapped values.
 
 #endif /* COORDINATE_HANDLER_H_ */
