e79a599c593578935765d15c19706282193841b3
Jrampoldi <rampoldijourdan@gmail.com>
2024-01-14 20:48:28 -0600

    Found logic error with radians. Fixed in coordinate_handler.h

--- ServoClock/Debug/Src/main.d
+++ ServoClock/Debug/Src/main.d
@@ -6,8 +6,8 @@ Src/main.o: ../Src/main.c \
  /Users/jourdanrampoldi/workspace/CMSIS/Include/cmsis_gcc.h \
  /Users/jourdanrampoldi/workspace/CMSIS/Include/mpu_armv7.h \
  /Users/jourdanrampoldi/workspace/CMSIS/Device/ST/STM32F4xx/system_stm32f4xx.h \
- ../Inc/pwm_handler.h ../Inc/tim_handler.h ../Inc/coordinate_handler.h \
- ../Inc/pwm_handler.h ../Inc/UART_handler.h
+ ../Inc/pwm_handler.h ../Inc/uart_handler.h ../Inc/tim_handler.h \
+ ../Inc/coordinate_handler.h ../Inc/pwm_handler.h ../Inc/UART_handler.h
 /Users/jourdanrampoldi/workspace/CMSIS/Device/ST/STM32F4xx/stm32f411xe.h:
 /Users/jourdanrampoldi/workspace/CMSIS/Include/core_cm4.h:
 /Users/jourdanrampoldi/workspace/CMSIS/Include/cmsis_version.h:
@@ -16,6 +16,7 @@ Src/main.o: ../Src/main.c \
 /Users/jourdanrampoldi/workspace/CMSIS/Include/mpu_armv7.h:
 /Users/jourdanrampoldi/workspace/CMSIS/Device/ST/STM32F4xx/system_stm32f4xx.h:
 ../Inc/pwm_handler.h:
+../Inc/uart_handler.h:
 ../Inc/tim_handler.h:
 ../Inc/coordinate_handler.h:
 ../Inc/pwm_handler.h:
