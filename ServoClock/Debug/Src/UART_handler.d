a12bc1e089c1aab8e70afa1edb694265b33378ec
Jrampoldi <rampoldijourdan@gmail.com>
2024-01-14 19:00:33 -0600

    Problem Identified with rad_to_degree. Fixed errors & FPU init

--- ServoClock/Debug/Src/UART_handler.d
+++ ServoClock/Debug/Src/UART_handler.d
@@ -1,13 +1,12 @@
-Src/main.o: ../Src/main.c \
+Src/UART_handler.o: ../Src/UART_handler.c ../Inc/UART_handler.h \
  /Users/jourdanrampoldi/workspace/CMSIS/Device/ST/STM32F4xx/stm32f411xe.h \
  /Users/jourdanrampoldi/workspace/CMSIS/Include/core_cm4.h \
  /Users/jourdanrampoldi/workspace/CMSIS/Include/cmsis_version.h \
  /Users/jourdanrampoldi/workspace/CMSIS/Include/cmsis_compiler.h \
  /Users/jourdanrampoldi/workspace/CMSIS/Include/cmsis_gcc.h \
  /Users/jourdanrampoldi/workspace/CMSIS/Include/mpu_armv7.h \
- /Users/jourdanrampoldi/workspace/CMSIS/Device/ST/STM32F4xx/system_stm32f4xx.h \
- ../Inc/pwm_handler.h ../Inc/tim_handler.h ../Inc/coordinate_handler.h \
- ../Inc/pwm_handler.h ../Inc/UART_handler.h
+ /Users/jourdanrampoldi/workspace/CMSIS/Device/ST/STM32F4xx/system_stm32f4xx.h
+../Inc/UART_handler.h:
 /Users/jourdanrampoldi/workspace/CMSIS/Device/ST/STM32F4xx/stm32f411xe.h:
 /Users/jourdanrampoldi/workspace/CMSIS/Include/core_cm4.h:
 /Users/jourdanrampoldi/workspace/CMSIS/Include/cmsis_version.h:
@@ -15,8 +14,3 @@ Src/main.o: ../Src/main.c \
 /Users/jourdanrampoldi/workspace/CMSIS/Include/cmsis_gcc.h:
 /Users/jourdanrampoldi/workspace/CMSIS/Include/mpu_armv7.h:
 /Users/jourdanrampoldi/workspace/CMSIS/Device/ST/STM32F4xx/system_stm32f4xx.h:
-../Inc/pwm_handler.h:
-../Inc/tim_handler.h:
-../Inc/coordinate_handler.h:
-../Inc/pwm_handler.h:
-../Inc/UART_handler.h:
--- ServoClock/Debug/Src/subdir.mk
+++ ServoClock/Debug/Src/subdir.mk
@@ -5,6 +5,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../Src/UART_handler.c \
 ../Src/coordinate_handler.c \
 ../Src/main.c \
 ../Src/pwm_handler.c \
@@ -13,6 +14,7 @@ C_SRCS += \
 ../Src/tim_handler.c 
 
 OBJS += \
+./Src/UART_handler.o \
 ./Src/coordinate_handler.o \
 ./Src/main.o \
 ./Src/pwm_handler.o \
@@ -21,6 +23,7 @@ OBJS += \
 ./Src/tim_handler.o 
 
 C_DEPS += \
+./Src/UART_handler.d \
 ./Src/coordinate_handler.d \
 ./Src/main.d \
 ./Src/pwm_handler.d \
@@ -31,12 +34,12 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F411CEUx -c -I../Inc -I/Users/jourdanrampoldi/workspace/CMSIS/Include -I/Users/jourdanrampoldi/workspace/CMSIS/Device/ST/STM32F4xx -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4=1 -DSTM32 -DSTM32F4 -DSTM32F411CEUx -c -I../Inc -I/Users/jourdanrampoldi/workspace/CMSIS/Include -I/Users/jourdanrampoldi/workspace/CMSIS/Device/ST/STM32F4xx -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/coordinate_handler.cyclo ./Src/coordinate_handler.d ./Src/coordinate_handler.o ./Src/coordinate_handler.su ./Src/main.cyclo ./Src/main.d ./Src/main.o ./Src/main.su ./Src/pwm_handler.cyclo ./Src/pwm_handler.d ./Src/pwm_handler.o ./Src/pwm_handler.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su ./Src/tim_handler.cyclo ./Src/tim_handler.d ./Src/tim_handler.o ./Src/tim_handler.su
+	-$(RM) ./Src/UART_handler.cyclo ./Src/UART_handler.d ./Src/UART_handler.o ./Src/UART_handler.su ./Src/coordinate_handler.cyclo ./Src/coordinate_handler.d ./Src/coordinate_handler.o ./Src/coordinate_handler.su ./Src/main.cyclo ./Src/main.d ./Src/main.o ./Src/main.su ./Src/pwm_handler.cyclo ./Src/pwm_handler.d ./Src/pwm_handler.o ./Src/pwm_handler.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su ./Src/tim_handler.cyclo ./Src/tim_handler.d ./Src/tim_handler.o ./Src/tim_handler.su
 
 .PHONY: clean-Src
 
